8c07b6364b69bbe04b5b8334d1705927416317ac
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-01-18 13:00:41 +0100

    Add files via upload

---
+++ month kalendar kontrola/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace month_kalendar_kontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.buttontermin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.Location = new System.Drawing.Point(289, 45);
+            this.monthCalendar.MaxSelectionCount = 14;
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.TabIndex = 0;
+            this.monthCalendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar_DateChanged);
+            // 
+            // buttontermin
+            // 
+            this.buttontermin.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttontermin.Location = new System.Drawing.Point(313, 264);
+            this.buttontermin.Name = "buttontermin";
+            this.buttontermin.Size = new System.Drawing.Size(207, 46);
+            this.buttontermin.TabIndex = 1;
+            this.buttontermin.Text = "odaberite termin";
+            this.buttontermin.UseVisualStyleBackColor = true;
+            this.buttontermin.Click += new System.EventHandler(this.buttontermin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttontermin);
+            this.Controls.Add(this.monthCalendar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+        private System.Windows.Forms.Button buttontermin;
+    }
+}
+
---
+++ month kalendar kontrola/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace month_kalendar_kontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void buttontermin_Click(object sender, EventArgs e)
+        {
+            TimeSpan odabir = monthCalendar.SelectionEnd - monthCalendar.SelectionStart;
+
+            if(odabir.Days < 1)
+            {
+                MessageBox.Show("Niste odabrali niti jedan datum stisnite shift za odabranje datuma: " , "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Vaša rezervacija pocinje od: " + monthCalendar.SelectionStart.ToShortDateString() + " do" + monthCalendar.SelectionEnd.ToShortDateString(), "Greska", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+    }
+}
